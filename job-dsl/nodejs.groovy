0379902cac403c6a81b5438f739d573257349044
Mahesh Boini <80108387+mboininc@users.noreply.github.com>
2022-01-29 19:14:12 +0530

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('MahNodeJs example') {
     scm {
-        git('https://github.com/mboininc/jenkins-course.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('https://github.com/mboininc/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
